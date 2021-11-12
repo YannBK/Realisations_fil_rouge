9d30aff32cc010db3127b2bd0efa4ee50ab2e6bd
YannBK <61941063+YannBK@users.noreply.github.com>
2021-11-12 08:55:47 +0100

    liens et buttons; login

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Realisations_fil_rouge
+Projet personnel réalisé dans le cadre de ma formation Développeur web / mobile effectuée à l'ADRAR Occitanie de Septembre 2021 à Juin 2022.
--- Réalisation_2__HTML__CSS/js/popup-save.js
+++ Réalisation_2__HTML__CSS/js/popup-save.js
@@ -1,41 +1,29 @@
 
 // popup SAVE
-const btnsSave = document.querySelectorAll(".save");
-const btnCloseSave = document.querySelector(".close-save");
-const btnResetSave = document.querySelector(".reset-save");
+const BTNS_Save = document.querySelectorAll(".save");
+const BTN_CloseSave = document.querySelector(".close-save");
+const BTN_ResetSave = document.querySelector(".reset-save");
+const DIODES = document.querySelectorAll(".diode");
+const BTN_CloseDiode = document.querySelector(".close-diode");
 
-function openSave() {
-	let formSave = document.querySelector(".popup-save");
+
+// open / close popup
+function openPopup(str) {
+	let formSave = document.querySelector(str);
 	formSave.style.display = "block";
 }
 
-function closeSave() {
-	let formSave = document.querySelector(".popup-save");
+function closePopup(str) {
+	let formSave = document.querySelector(str);
 	formSave.style.display = "none";
 }
 
-btnsSave.forEach(btnSave => btnSave.addEventListener("click", openSave));
-btnCloseSave.addEventListener("click", closeSave);
-btnResetSave.addEventListener("click", closeSave);
-
-// popup diode (non, ce n'est pas du copié/collé, m'enfin!)
-
-const diodes = document.querySelectorAll(".diode");
-const btnCloseDiode = document.querySelector(".close-diode");
-
-function openDiode() {
-	let formDiode = document.querySelector(".popup-diode");
-	formDiode.style.display = "block";
-}
-
-function closeDiode() {
-	let formDiode = document.querySelector(".popup-diode");
-	formDiode.style.display = "none";
-}
-
-diodes.forEach(diode => diode.addEventListener("click", openDiode));
-btnCloseDiode.addEventListener("click", closeDiode);
-
-
+// Event Listener popup Save
+BTNS_Save.forEach(btnSave => btnSave.addEventListener("click", ()=>openPopup(".popup-save")));
+BTN_CloseSave.addEventListener("click", ()=>closePopup(".popup-save"));
+BTN_ResetSave.addEventListener("click", ()=>closePopup(".popup-save"));
+// Event Listener popup Diodes
+DIODES.forEach(diode => diode.addEventListener("click", ()=>openPopup(".popup-diode")));
+BTN_CloseDiode.addEventListener("click", ()=>closePopup(".popup-diode"));
 
 
