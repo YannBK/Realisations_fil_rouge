0e54b4d6a59e0fcae9de5a868da75dbaa703deae
YannBK <61941063+YannBK@users.noreply.github.com>
2021-11-18 20:00:58 +0100

    degraissage

---
+++ Réalisation_2__HTML__CSS/js/modal-save.js
@@ -0,0 +1,58 @@
+
+// Modal SAVE
+const BTNS_Save = document.querySelectorAll(".save");
+const BTN_CloseSave = document.querySelector(".close-save");
+const BTN_ResetSave = document.querySelector(".reset-save");
+const DIODES = document.querySelectorAll(".diode");
+const BTN_CloseDiode = document.querySelector(".close-diode");
+const CASES = document.querySelectorAll(".cases");
+const BTN_CloseCase = document.querySelector(".close-case");
+const DIODES_H = document.querySelectorAll(".diodeH");
+const BTN_CloseDiodeCustom = document.querySelectorAll(".close-custom");
+
+// open / close Modal
+function openModal(str) {
+	let formPop = document.querySelector(str);
+	formPop.style.display = "block";
+}
+
+function closeModal(str) {
+	let formPop = document.querySelector(str);
+	formPop.style.display = "none";
+}
+
+// Event Listener modal Save
+if(BTNS_Save.length>0){
+	BTNS_Save.forEach(btnSave => btnSave.addEventListener("click", ()=>openModal(".modal-save")));
+	BTN_CloseSave.addEventListener("click", ()=>closeModal(".modal-save"));
+	BTN_ResetSave.addEventListener("click", ()=>closeModal(".modal-save"));
+} else {
+	false;
+}
+// Event Listener modal Diodes
+if(DIODES.length>0){
+	DIODES.forEach(diode => diode.addEventListener("click", ()=>openModal(".modal-diode")));
+	BTN_CloseDiode.addEventListener("click", ()=>closeModal(".modal-diode"));
+} else {
+	false;
+}
+// Event Listener modal Cases
+if(CASES.length>0){
+	CASES.forEach(cas => cas.addEventListener("click", ()=>openModal(".modal-case")));
+	BTN_CloseCase.addEventListener("click", ()=>closeModal(".modal-case"));
+} else {
+	false;
+}
+// event listener diodes custom
+if(DIODES_H.length>0){
+	DIODES_H.forEach(diode => diode.addEventListener("click", ()=>openModal(".diode-custom")));
+	BTN_CloseDiodeCustom.forEach(btn => btn.addEventListener("click", ()=>closeModal(".diode-custom")));
+	//ajouter display:none aux box-titoir à la fermeture
+} else {
+	false;
+}
+
+
+
+
+
